0cfee5ffb6ee5721b5629071334514c47adf19b9
goobeom.jeoung <creator.koo@icloud.com>
2020-12-10 10:33:50 +0900

    When unlocking, Add vault sync step

--- unlock.sh
+++ unlock.sh
@@ -7,6 +7,8 @@ export BW_SESSION="$(bw unlock --raw)"
 bw unlock --check 2>/dev/null >/dev/null
 [[ $? -ne 0 ]] && exit 1
 
+bw sync
+
 rm -rf $SESSION_PATH
 touch $SESSION_PATH
 chmod 600 $SESSION_PATH
