a66e24d8cc078f19a7bc1f44d3f76f00a5f5efc7
goobeom.jeoung <creator.koo@icloud.com>
2021-02-08 14:26:21 +0900

    support to add ssh process option argument

--- ssh_connect.sh
+++ ssh_connect.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-if [ "$#" -ne 1 ]; then
-	echo "$0 <bitwarden item name>"
+if [ "$#" -lt 1 ]; then
+	echo "$0 <bitwarden item name> [ssh option argument]"
 	exit 1
 fi
 
 source ~/bitwarden-iterm-helper/unlock.sh
 
 BW_ITEM_NAME=$1
+SSH_OPTION="${@:2}"
 ITEM_JSON=$(bw list items --search "$BW_ITEM_NAME" | jq --raw-output ".[] | select( .name == \"$BW_ITEM_NAME\")")
 
 USERNAME=$(echo $ITEM_JSON | jq --raw-output .login.username)
 URL=$(echo $ITEM_JSON | jq --raw-output .login.uris[0].uri)
 
-ssh $USERNAME@$URL
+ssh $USERNAME@$URL $SSH_OPTION
