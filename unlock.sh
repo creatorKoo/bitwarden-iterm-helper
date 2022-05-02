108e635d8b45a741f3dec525fcbd917911237dcc
goobeom.jeoung <creator.koo@sk.com>
2022-05-02 12:00:27 +0900

    Add workaround for bitwarden/cli#480

--- unlock.sh
+++ unlock.sh
@@ -2,14 +2,26 @@
 
 source ~/bitwarden-iterm-helper/config.sh
 
-export BW_SESSION="$(bw unlock --raw)"
+BW_STATUS=$(bw status 2>/dev/null | jq -r '.status')
 
-bw unlock --check 2>/dev/null >/dev/null
-[[ $? -ne 0 ]] && exit 1
+if [[ "$BW_STATUS" == 'locked' ]]; then
+	export BW_SESSION=$(bw unlock --raw)
+	
+	# workaround for https://github.com/bitwarden/cli/issues/480
+	if [ bw list folders --nointeraction --session "$BW_SESSION" >/dev/null 2>&1 ]; then
+		echo 'Unlocking Fail'
+		sleep 2
+		exit -1
+	fi
+	# end workaround
+	bw sync
 
-bw sync
-
-rm -rf $SESSION_PATH
-touch $SESSION_PATH
-chmod 600 $SESSION_PATH
-echo $BW_SESSION >> $SESSION_PATH
+	rm -rf $SESSION_PATH
+	touch $SESSION_PATH
+	chmod 600 $SESSION_PATH
+	echo $BW_SESSION >> $SESSION_PATH
+elif [[ $BW_STATUS == 'unauthenticated' ]]; then
+	echo 'You are not logged in.'
+	sleep 2
+	exit -1
+fi
