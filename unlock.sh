c894f65fdcc6b6a9ac886b1d71e49e20917c87f5
goobeom.jeoung <creator.koo@icloud.com>
2022-10-14 11:31:48 +0900

    fix: delete bitwarden-cli workaround

--- unlock.sh
+++ unlock.sh
@@ -6,14 +6,12 @@ BW_STATUS=$(bw status 2>/dev/null | jq -r '.status')
 
 if [[ "$BW_STATUS" == 'locked' ]]; then
 	export BW_SESSION=$(bw unlock --raw)
-	
-	# workaround for https://github.com/bitwarden/cli/issues/480
-	if [ bw list folders --nointeraction --session "$BW_SESSION" >/dev/null 2>&1 ]; then
+	if [ bw unlock --quiet --check --session "$BW_SESSION" >/dev/null 2>&1 ]; then
 		echo 'Unlocking Fail'
 		sleep 2
 		exit -1
 	fi
-	# end workaround
+
 	bw sync
 
 	rm -rf $SESSION_PATH
